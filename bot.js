3069518f94c6cf0de320bf5e06df398d0b5ed569
alserh <a.serheyuk@gmail.com>
2024-07-12 23:42:25 +0300

    error logging

--- bot.js
+++ bot.js
@@ -8,11 +8,24 @@ const bot = new Bot(`${tgToken}`);
 let i = 0;
 
 bot.on("message", (ctx) => {
-    
-    i++
+
+    i++;
     slackPostMessage(ctx);
-    console.log(`Sent ${i} messages to slack`)
+    console.log(`Sent ${i} messages to slack`);
     
+});
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
 });
 
 bot.start();
